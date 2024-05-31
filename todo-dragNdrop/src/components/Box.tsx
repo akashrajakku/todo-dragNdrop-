9cc64688e4f2ac494a8ee52cf4087d2388397b19
akashrajakku <akashrajakku263@gmail.com>
2024-05-31 19:47:50 +0530

    upto add course button

--- todo-dragNdrop/src/components/Box.tsx
+++ todo-dragNdrop/src/components/Box.tsx
@@ -2,7 +2,29 @@
 
 function Box() {
   return (
-    <div>Box</div>
+    <div 
+    className="
+    m-auto
+    flex
+    min-h-screen
+    w-full
+    items-center
+    overflow-x-auto
+    overflow-y-hidden
+    px-[40px]
+    "
+    >
+      <div className="m-auto">
+      
+    <button className="flex items-center px-4 py-2 bg-blue-500 text-white text-lg font-semibold rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+        Add Course 
+        <svg className="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M12 4v16m8-8H4"></path>
+        </svg>
+        
+    </button>
+    </div>
+    </div>
   )
 }
 
